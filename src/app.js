5a948f5b61b222c99e6bbf529b375309fe8cf964
saulo <saulodavi2001@gmail.com>
2022-10-19 16:32:54 -0300

    so começando

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@ export default function App(){
     return(
             <BrowserRouter>
                 <Routes>
-                    <Route path='/' element={<Home/>}/>
-                    <Route path='/timeline' element={<Home/>}/>
+                    {/* <Route path='/' element={</>}/>
+                    <Route path='/timeline' element={</>}/> */}
                 </Routes>
             </BrowserRouter>
     )
